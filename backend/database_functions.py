91cc929cbc23c57836bf49251e2476e22c54eb7f
Anurag A S <sampathanurag3@gmail.com>
2019-08-06 22:44:45 +0530

    Querying User Records

--- backend/database_functions.py
+++ backend/database_functions.py
@@ -4,12 +4,14 @@ import sqlite3
 # Connecting to the database
 def connection():
     db_connection = sqlite3.connect('costracjectory.db')
+    db_connection.commit()
     return db_connection
 
 
 # Closing the connection to a database
 def end_connection(db_connection):
     db_connection.close()
+    db_connection.commit()
 
 
 # Creating the user and password table
@@ -19,13 +21,23 @@ def create_user_table(db_connection):
          username           TEXT    NOT NULL,
          password           TEXT     NOT NULL);''')
     print("User Table created successfully")
+    db_connection.commit()
 
 
 # Inserting Entry into User Table
 def insert_into_user_table(db_connection, username, password):
-    db_connection.execute('''INSERT INTO USERS (username, password) VALUES ("username","password")'''
+    db_connection.execute('''INSERT INTO USERS (username, password) VALUES ("{username}","{password}")'''
                           .format(username=username, password=password))
     print("User entry inserted into table")
+    db_connection.commit()
+
+
+# Querying all users from the user table
+def query_all_records(db_connection):
+    cursor = db_connection.execute("SELECT * FROM USERS")
+    for row in cursor:
+        print(row[1])
+    db_connection.commit()
 
 
 db = connection()
