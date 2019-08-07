e26af2a2371eae2a82b35580ee616a15065f45fc
Anurag A S <sampathanurag3@gmail.com>
2019-08-07 19:16:10 +0530

    Added inserting user into user table

--- backend/query_signup.py
+++ backend/query_signup.py
@@ -4,10 +4,18 @@ from database_functions import *
 class SignUp:
 
     # Initializing the connection
-    def __init__(self, username):
+    def __init__(self, username, password):
         self.db = connection()
         self.user = username
+        self.password = password
 
+    # check if user exists
     def check_user(self):
         return user_exists(self.db, self.user)
 
+    # add a user into user table
+    def add_user(self):
+        if not self.check_user():
+            insert_into_user_table(self.db, self.user, self.password)
+            return True
+        return False
