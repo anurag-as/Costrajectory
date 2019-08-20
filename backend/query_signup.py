9d71cd9444f546734db7f88f39479d65c265189b
Anurag A S <sampathanurag3@gmail.com>
2019-08-20 21:32:03 +0530

    Added API for Adding User

--- backend/query_signup.py
+++ backend/query_signup.py
@@ -4,7 +4,7 @@ from database_functions import *
 class SignUp:
 
     # Initializing the connection
-    def __init__(self, username, password):
+    def __init__(self, username, password=None):
         self.db = connection()
         self.user = username
         self.password = password
@@ -13,9 +13,17 @@ class SignUp:
     def check_user(self):
         return user_exists(self.db, self.user)
 
+    # adding password to the SignUp Object
+    def add_password(self, password):
+        self.password = password
+
+    # check if password is not empty
+    def check_password(self):
+        return self.password
+
     # add a user into user table
     def add_user(self):
-        if not self.check_user():
+        if not self.check_user() and self.check_password():
             insert_into_user_table(self.db, self.user, self.password)
             return True
         return False
