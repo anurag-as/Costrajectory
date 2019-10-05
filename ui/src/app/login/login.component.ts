925d1232ddc200c08c2b505b6480a5997141ee87
Rohit P <rohitp2512@gmail.com>
2019-10-05 19:10:36 +0530

    SignUp page with Validator (No Routing)

---
+++ ui/src/app/login/login.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  username: any = '';
+  registerForm: FormGroup;
+  pwd1: any = '';
+  pwd2: any = '';
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  private MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    };
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+  }, {
+      validator: this.MustMatch('password', 'confirmPassword')
+  });
+  }
+
+  private onCanSubmit() {
+    if ( this.username !== '' && this.pwd1 !== '' && this.pwd2 !== '') {
+      return( true );
+    } else {
+      return( false );
+    }
+  }
+
+  get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value));
+    }
+
+
+}
