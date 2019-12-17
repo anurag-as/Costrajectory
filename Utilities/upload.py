1bfb292f9aa865cb5afcc153ac97b031df2773f1
Rakesh Gourani <rakeshgourani1740@gmail.com>
2019-12-17 23:17:36 +0530

    Added utilities to upload database in overwrite mode to dropbox and download the database

---
+++ Utilities/upload.py
@@ -0,0 +1,17 @@
+#Uploads costracjectory.db to dropbox
+
+import dropbox
+import os
+from dropbox.files import WriteMode
+
+token = 'UVXGVWSMloAAAAAAAAAAYH3ZFioGbCYRCJGNDTnYGkFy0-qmsh9AwB-BofQ14gW2'
+
+def upload():
+ dbx = dropbox.Dropbox(token)
+ file_name = os.getcwd().rstrip('Utilities')+'backend\\costracjectory.db'
+ f = open(file_name, 'rb')
+ dbx.files_upload(f.read(), '/costracjectory.db', mode=WriteMode('overwrite'))
+ print(file_name + ' Uploaded')
+
+if __name__ == "__main__":
+ upload()
