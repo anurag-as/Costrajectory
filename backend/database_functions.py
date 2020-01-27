4c4bf1e9dbbe864db5c24097f030c69c46b3a0a7
Anurag A S <sampathanurag3@gmail.com>
2020-01-27 21:20:30 +0530

    Added Token Saving to DB TODO for Signup

--- backend/database_functions.py
+++ backend/database_functions.py
@@ -90,3 +90,33 @@ def insert_into_image_table(db_connection, username, datetime, description):
     "{description}")'''.format(username=username, datetime=datetime, description=description))
     print("Image entry inserted into table")
     db_connection.commit()
+
+
+# function to create token database
+def create_token_table(db_connection):
+    db_connection.execute('''CREATE TABLE TOKENS
+         (ID INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL ,
+         username           TEXT    NOT NULL,
+         datetime           TEXT    NOT NULL,
+         token        TEXT    NOT NULL);''')
+    print("Token Table created successfully")
+    db_connection.commit()
+
+
+# function to insert tokens into table
+def insert_into_token_table(db_connection, username, datetime, token):
+    db_connection.execute('''INSERT INTO TOKENS (username, datetime, token) VALUES ("{username}","{datetime}",
+    "{token}")'''.format(username=username, datetime=datetime, token=token))
+    print("Token entry inserted into table")
+    db_connection.commit()
+
+
+
+# function to get the hashed password for the particular user
+def get_datetime_token(db_connection, username, token):
+    cursor = db_connection.execute('''SELECT datetime FROM TOKEN where username = "{username}" AND token = "{token}"
+    LIMIT 1'''.format(username=username, token=token))
+    for row in cursor:
+        if row:
+            return row[0]
+    return False
