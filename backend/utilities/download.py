e29232f02893cea6de42f6ccb54f199892490ae4
Anurag A S <sampathanurag3@gmail.com>
2020-01-30 20:18:17 +0530

    Added YML Configuration File and Issue #34

--- backend/utilities/download.py
+++ backend/utilities/download.py
@@ -2,15 +2,25 @@
 
 import dropbox
 import os
+import yaml
 from dropbox.files import WriteMode
 
-token = 'UVXGVWSMloAAAAAAAAAAYH3ZFioGbCYRCJGNDTnYGkFy0-qmsh9AwB-BofQ14gW2'
+with open("..//..//config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+token = cfg['token']['dropbox']
 
 
 def download():
     dbx = dropbox.Dropbox(token)
-    dbx.files_download_to_file(os.getcwd().rstrip("utilities") + 'costrajectory.db',
+    if cfg['environment']['host'] == 'linux':
+        print("Linux Environment")
+        dbx.files_download_to_file(os.getcwd().rstrip("utilities") + 'costrajectory.db',
                                '/costrajectory.db')
+    else:
+        print("Windows Environment")
+        dbx.files_download_to_file(os.getcwd() + '\\costrajectory.db',
+                                   '/costrajectory.db')
     print('DB Successfully updated to remote')
 
 
