a24d843e07eb1f46bd30e11effdebccef39a5979
Anurag A S <sampathanurag3@gmail.com>
2020-02-06 19:35:04 +0530

    Added DB Support for image mapping

--- backend/database_functions.py
+++ backend/database_functions.py
@@ -133,9 +133,19 @@ def create_image_mapping_table(db_connection):
 
 
 # function to insert into IMAGEMAPPING into table
-def insert_into_token_table(db_connection, username, original, mapped):
+def insert_into_image_mapping_table(db_connection, username, original, mapped):
     db_connection.execute('''INSERT INTO IMAGEMAPPING (username, original_name, mapped_name) VALUES ("{username}","{original}",
     "{mapped}")'''.format(username=username, original=original, mapped=mapped))
     print("Image Mapping entry inserted into table")
     db_connection.commit()
 
+
+# function to get the original image name from the mapped name
+def get_original_name(db_connection, username, mapped_name):
+    cursor = db_connection.execute('''SELECT original_name FROM IMAGEMAPPING where username = "{username}" AND mapped_name
+     = "{mapped}"
+    LIMIT 1'''.format(username=username, mapped=mapped_name))
+    for row in cursor:
+        if row:
+            return row[0]
+    return False
