edc1e4fc8b4915c6ef9718162aab9d92a1307fd3
Anurag A S <sampathanurag3@gmail.com>
2020-03-06 20:39:18 +0530

    Added dropbox functionality to delete an image

---
+++ backend/utilities/delete_file.py
@@ -0,0 +1,21 @@
+# Downloads costrajectory.db from dropbox and save it in backend as costrajectory_downloaded.db
+
+import dropbox
+import os
+import yaml
+from dropbox.files import WriteMode
+from utils import *
+
+root_directory = get_root_directory()
+
+with open(os.path.join(root_directory, "config.yml"), 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, yaml.SafeLoader)
+
+token = cfg['token']['dropbox']
+
+
+def delete_file(mapped_image_name):
+    dbx = dropbox.Dropbox(token)
+    dbx.files_delete_v2('/' + mapped_image_name)
+
+
