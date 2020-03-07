c11cba2a17d7207299c395ce59d3e7faf2ececb6
Rohit Prasad <rohitp2512@gmail.com>
2020-03-07 19:54:16 +0530

    Added analytics

--- ui/src/app/analytics/analytics.component.ts
+++ ui/src/app/analytics/analytics.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import * as CanvasJS from '.././canvasjs.min';
+import {GetAllTransactionDetails} from './GetAllTransactionDetails.service';
+import {GlobalConfigsService} from '../global-configs.service';
+
+interface BillData {
+  username: string;
+  TableEntries: [];
+  ImageEntries: [];
+}
 
 @Component({
   selector: 'app-analytics',
@@ -7,37 +15,103 @@ import * as CanvasJS from '.././canvasjs.min';
   styleUrls: ['./analytics.component.css']
 })
 export class AnalyticsComponent implements OnInit {
+  BillEntries = [];
+  DataLoading = 'Started';
+  UserName: string;
+  FormData: BillData;
+  Categories = ['Travel','Shopping','Investments','Food','Utilities','Medical','Entertainment','Housing','Others']
+  DatainValue = {
+    'Travel':0,
+    'Shopping':0,
+    'Investments':0,
+    'Food':0,
+    'Utilities':0,
+    'Medical':0,
+    'Entertainment':0,
+    'Housing':0,
+    'Others':0,
+  };
+  DatainPercent= {
+    'Travel':0,
+    'Shopping':0,
+    'Investments':0,
+    'Food':0,
+    'Utilities':0,
+    'Medical':0,
+    'Entertainment':0,
+    'Housing':0,
+    'Others':0,
+  };
+  ValueArray = [];
+  ConsolidatedValue: any = [];
+  ConsolidatedPecent: any = [];
 
-  constructor() { }
+  TotalSum: any;
+  constructor(private DataGather: GetAllTransactionDetails, public globals: GlobalConfigsService) { }
 
   ngOnInit() {
-    this.chartRender();
+    this.DataGather.GetData( this.globals.GetUserName ).subscribe( data => {
+      // console.log('MAIN DATA : ', data);
+      this.DataLoading = 'Success';
+      this.FormData = data;
+      
+      for ( const entry of data.TableEntries) {
+        this.DatainValue[entry.category] += parseInt(entry.Amount);
+        this.ValueArray.push(parseInt(entry.Amount));
+      }
+
+      this.TotalSum = this.SumArray(this.ValueArray)
+
+      for ( const category of this.Categories) {
+        this.ConsolidatedValue.push({ label: category, y: this.DatainValue[category] });
+        this.ConsolidatedPecent.push({ label: category, y: (parseInt(this.DatainValue[category]) / this.TotalSum) * 100 })
+      }
+
+      this.BarChartRender(this.ConsolidatedValue, this.Categories);
+      this.PieChartRender(this.ConsolidatedPecent, this.Categories);
+
+    }, err => {
+      this.DataLoading = 'Fail';
+    });
+    
   }
 
-  chartRender() {
-    const chart = new CanvasJS.Chart('chartContainer', {
+  SumArray(arr): Number {
+    return(arr.reduce((a, b) => a + b, 0))
+  }
+
+  BarChartRender(ConsolidatedValue, Categories) {
+    const chart = new CanvasJS.Chart('chartContainerBar', {
       animationEnabled: true,
       exportEnabled: true,
       title: {
-        text: 'Basic Column Chart in Angular'
+        text: ''
       },
       data: [{
         type: 'column',
-        dataPoints: [
-          { y: 71, label: 'Apple' },
-          { y: 55, label: 'Mango' },
-          { y: 50, label: 'Orange' },
-          { y: 65, label: 'Banana' },
-          { y: 95, label: 'Pineapple' },
-          { y: 68, label: 'Pears' },
-          { y: 28, label: 'Grapes' },
-          { y: 34, label: 'Lychee' },
-          { y: 14, label: 'Jackfruit' }
-        ]
+        dataPoints: ConsolidatedValue
       }]
     });
 
     chart.render();
       }
+
+    PieChartRender(ConsolidatedPecent, Categories) {
+      // console.log('DATAPOINTS : ',ConsolidatedPecent, Categories, this.DatainValue, this.TotalSum );
+      var chart = new CanvasJS.Chart("chartContainerPie", {
+        animationEnabled: true,
+        title: {
+          text: ""
+        },
+        data: [{
+          type: "pie",
+          startAngle: 240,
+          yValueFormatString: "##0.00\"%\"",
+          indexLabel: "{label} {y}",
+          dataPoints: ConsolidatedPecent
+        }]
+      });
+      chart.render();
+    }
 
 }
