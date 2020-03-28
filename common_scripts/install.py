a6b33718574c773dde239f8e9db2ba5bd86de0e1
Anurag A S <sampathanurag3@gmail.com>
2020-03-28 13:04:11 +0530

    Enhanced the automation script

--- common_scripts/install.py
+++ common_scripts/install.py
@@ -1,10 +1,55 @@
 from subprocess import Popen
-from os import getcwd, path
+from os import getcwd, path, uname
+from yaml import dump
+import io
 
-# Installing python library requirements
-pythonic_library_install = Popen(['pip3', 'install', '-r', 'requirements.txt'], cwd=getcwd())
-pythonic_library_install.wait()
 
-# Installing node modules
-node_directory = path.join(getcwd().split('common_scripts')[0], 'ui')
-node_modules_install = Popen(['npm', 'install'], cwd=node_directory, shell=True)
+def python_library():
+    # Installing python library requirements
+    python_library_install = Popen(['pip3', 'install', '-r', 'requirements.txt'], cwd=getcwd())
+    python_library_install.wait()
+
+
+def node_library():
+    # Installing node modules
+    node_directory = path.join(getcwd().split('common_scripts')[0], 'ui')
+    node_modules_install = Popen(['npm', 'install'], cwd=node_directory, shell=True)
+    node_modules_install.wait()
+
+
+def get_params():
+    params = list(uname())[:2]
+    return params
+
+
+def config():
+    # adding the config.yaml file for the project
+    params = get_params()
+    system = params[0].lower()
+    username = params[1]
+    yaml = dict(
+        environment=dict(
+            host=system,
+            user=username
+        ),
+        token=dict(
+            dropbox='Contact project lead'
+        ),
+        server=dict(
+            limit=dict(
+                premium=20.0,
+                normal=10.0
+            )
+        )
+    )
+
+    yaml_directory = path.join(getcwd().split('common_scripts')[0], 'config.yml')
+
+    with open(yaml_directory, 'w') as outfile:
+        dump(yaml, outfile, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    python_library()
+    node_library()
+    config()
