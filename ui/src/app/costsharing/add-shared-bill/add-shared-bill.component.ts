17c3cf3509f4a6fce5cc061e736520e3dbd03a8e
Rohit Prasad <rohitp2512@gmail.com>
2020-04-18 11:17:03 +0530

    Added the Image, share toggler, waiting for backend support

--- ui/src/app/costsharing/add-shared-bill/add-shared-bill.component.ts
+++ ui/src/app/costsharing/add-shared-bill/add-shared-bill.component.ts
@@ -21,28 +21,13 @@ export class AddSharedBillComponent implements OnInit {
   ValueMapper = Array();
   IsShowingSharing = true;
 
-  username = undefined;
-  BillName = '';
-  BillDescription = '';
-  BillAmount: string = undefined;
-  BillEnum: any = undefined;
-  BillHasImage: boolean = undefined;
-  BillID = undefined;
-  BillIdentifier: string = undefined;
-  BillImage: any = undefined;
-  BillDate: any = undefined;
-  BillCategory: any = undefined;
-  MappedImageName = undefined;
-  ActualImageName = undefined;
-  imageToShow = undefined;
-  canShowImage = true;
-  base64Data = undefined;
-  uploading = 'not started';
-  fileToUpload =  this.ActualImageName;
-  imgfromServer = true;
-  canShowImageUploaded = false;
-  imageUploaded = false;
+  datetoday = new Date();
   imageSrc;
+  CurrentForm;
+  uploading = 'not started';
+  fileToUpload: File = null;
+  username = undefined;
+  currentDate;
 
   constructor(private http: HttpClient) { }
 
@@ -53,50 +38,17 @@ export class AddSharedBillComponent implements OnInit {
     }
   }
 
-  receiveImage(URL: string, Payload: any) {
-    return this.http.post<ReturnImage>(URL, Payload);
-  }
-
   ChangeMode() {
     this.IsShowingSharing = ! this.IsShowingSharing;
   }
 
-  getImage() {
-    const endpoint = 'http://127.0.0.1:5000/previewImage';
-    const QueryPayload = {username: this.username, mapped_name : this.MappedImageName, original_name: this.ActualImageName};
-    this.receiveImage(endpoint, QueryPayload).subscribe(data => {
-      this.canShowImageUploaded = true;
-      this.imageSrc = data.Image;
-    });
-  }
-
-
   private handleFileInput(files: FileList) {
-    // console.log(this.fileToUpload);
     this.fileToUpload = files.item(0);
-
+    console.log(this.fileToUpload);
     const reader = new FileReader();
     reader.onload = e => this.imageSrc = reader.result;
 
     reader.readAsDataURL(this.fileToUpload);
-    this.imageUploaded = true;
-}
-
-
-clearImage() {
-  this.fileToUpload = null;
-  this.BillHasImage = false;
-  this.imageUploaded = false;
-}
-
-dataURLtoFile(arr, filename) {
-  const bstr = atob(arr);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], filename);
 }
 
 
