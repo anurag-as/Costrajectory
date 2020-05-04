ade9587cb1029401a1bbfb6b05f856f6c9c13960
Anurag A S <sampathanurag3@gmail.com>
2020-05-04 19:04:08 +0530

    Completed implementation of group space usage - Resolving JIRA COST-113

--- backend/api/analytics/usage.py
+++ backend/api/analytics/usage.py
@@ -2,12 +2,13 @@ from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
 from database_functions.db_connection.connection import connection
 from database_functions.account.token_auth_flow import refresh_token
-from database_functions.account.space_flow import space_usage
+from database_functions.account.space_flow import space_usage, group_space_usage
 from database_functions.account.premium_flow import is_user_premium
 
 from utilities.utils import get_total_size
 
 usageAPI = Blueprint('usageAPI', __name__)
+
 
 # API to get usage details for a particular user
 @usageAPI.route('/analytics/usage', methods=['POST'])
@@ -18,7 +19,7 @@ def user_space_usage():
     """
     user_name = request.json['username']
     refresh_token(connection(), user_name)
-    size = space_usage(connection(), user_name)
+    size = space_usage(connection(), user_name) + group_space_usage(connection(), user_name)
     bool_is_user_premium = is_user_premium(connection(), user_name)
     premium = False
     if bool_is_user_premium == 'True':
