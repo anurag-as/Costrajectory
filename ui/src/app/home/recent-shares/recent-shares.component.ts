6d77f50ddf0cf8b7f43ecb6dc898a7e9fcc88f69
Rohit Prasad <rohitp2512@gmail.com>
2020-06-07 16:56:41 +0530

    Added Random pick logic

--- ui/src/app/home/recent-shares/recent-shares.component.ts
+++ ui/src/app/home/recent-shares/recent-shares.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recent-shares',
@@ -6,15 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recent-shares.component.css']
 })
 export class RecentSharesComponent implements OnInit {
+  @Input() SharesPayload: any;
+  @Input() Username: string;
   testAlias = {
     'a@a.com': {alias: 'aa', name: false},
     'b@b.com': {alias: 'bb', name: false},
     'c@c.com': {alias: 'cc', name: false}
     };
+
+  /* COmbinations allowed currently -->
+    1. 2 settlement, 2 shares
+    2. 6 settlements
+    3. 1 settlement, 4 shares */
+  ContentQueueSettlement: any;
+  ContentQueuePay: any;
+  Settlements: number;
+  Shares: number;
+  FinalShares: any;
+  FinalSettlement: any;
   Share = ['c@c.com', 'b@b.com', 400];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  CreateCOntentFromPayload() {
+    for (const bill of this.SharesPayload) {
+      const tempQueueSettle = [];
+      const tempQueuePay = [];
+      for ( const share of bill.settlements) {
+        if (this.Username === share[1]) {
+          tempQueueSettle.push(share[0]);
+        } else if (this.Username === share[0]) {
+          tempQueuePay.push(share[1]);
+        }
+      }
+      this.ContentQueuePay.push(tempQueuePay);
+      this.ContentQueueSettlement.push(tempQueueSettle);
+    }
+  }
+
+  RandomSettlementSelector() {
+    // Initialize the numbers
+    this.Shares = Math.min( 2, this.ContentQueuePay.length);
+    this.Settlements = Math.min( 6 - this.Shares * 2, this.ContentQueueSettlement.length );
+
+    while (this.Shares > 0) {
+      const removedBit = this.ContentQueuePay.shift();
+      const removedElement = removedBit.shift();
+      this.FinalShares.push(removedElement);
+      this.Shares -= 1;
+      this.ContentQueuePay.push(removedBit);
+    }
+
+    while (this.Settlements > 0) {
+      const removedBit = this.ContentQueueSettlement.shift();
+      const removedElement = removedBit.shift();
+      this.FinalSettlement.push(removedElement);
+      this.Settlements -= 1;
+      this.ContentQueueSettlement.push(removedBit);
+    }
+
   }
 
 }
